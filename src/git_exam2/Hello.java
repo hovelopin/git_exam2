c8a2d4a29389d3ca052d813d6a54e7d28317e8f9
cohojin <kimhojin3714@naver.com>
2021-10-26 09:48:25 +0900

    201635007-5

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,9 +3,10 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201635007");
-		System.out.println("김호진");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201635007");
+		System.out.println("이름: 김호진");
 	}
 
 }
+
